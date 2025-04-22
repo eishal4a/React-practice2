75f9e50ac1c5cb00d2f98ee6cb436844b938a672
Eishal <eishal.cecos@gmail.com>
2025-04-22 08:18:50 +0500

    p
    p

--- my-react-app/src/greeting.jsx
+++ my-react-app/src/greeting.jsx
@@ -1,11 +1,24 @@
-function Greeting(props)
-{
-    return(
-        <div>
-            <h1>Hello,{props.name}!</h1>
-            <p>Welcome to React!</p>
-        </div>
-    );
+import { useState } from "react";
+
+function Greeting() {
+  const [name, setName] = useState("");
+
+  function handleInputChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Hello, {name || "friend"}!</h2>
+      <p>Welcome to React.</p>
+      <input 
+        type="text" 
+        placeholder="Enter your name" 
+        value={name}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
 }
 
 export default Greeting;
